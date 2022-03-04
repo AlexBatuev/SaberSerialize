e63044674568ac1968b33c27506d68db34d4c29e
Alexander Batuev <batuev.prizrak@gmail.com>
2022-03-04 17:47:31 +0900

    Добавьте файлы проекта.

---
+++ SaberSerialize/ListRand.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace SaberSerialize
+{
+    // Реализуйте функции сериализации и десериализации двусвязного списка, заданного следующим образом:
+    // Примечание: сериализация подразумевает сохранение и восстановление полной структуры списка,
+    // включая взаимное соотношение его элементов между собой — в том числе ссылок на Rand элементы.
+
+    class ListNode
+    {
+        public ListNode Prev;
+        public ListNode Next;
+        public ListNode Rand; // произвольный элемент внутри списка
+        public string Data;
+    }
+
+    class ListRand
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        public void Serialize(FileStream s)
+        {
+            var nodes = new List<ListNode>();
+            var current = Head;
+
+            while (current != null)
+            {
+                nodes.Add(current);
+                current = current.Next;
+            }
+
+            using var sw = new StreamWriter(s);
+            foreach (var node in nodes)
+            {
+                sw.WriteLine($"{node.Data}:{nodes.IndexOf(node.Rand)}");
+            }
+        }
+
+        public void Deserialize(FileStream s)
+        {
+            Count = 0;
+            var nodes = new List<ListNode>();
+            var current = new ListNode();
+            Head = current;
+
+            try
+            {
+                using (var sr = new StreamReader(s))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line != "")
+                        {
+                            Count++;
+                            current.Data = line;
+                            var next = new ListNode();
+                            current.Next = next;
+                            next.Prev = current;
+                            nodes.Add(current);
+                            current = next;
+                        }
+                    }
+                }
+
+                Tail = current.Prev;
+                Tail.Next = null;
+
+                foreach (var node in nodes)
+                {
+                    var separator = node.Data.LastIndexOf(':');
+                    var index = Convert.ToInt32(node.Data[(separator + 1)..]);
+                    node.Rand = index != -1 ? nodes[index] : null;
+                    node.Data = node.Data[..(separator)];
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Deserialization error!");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ SaberSerialize/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace SaberSerialize
+{
+    public class Program
+    {
+        private static readonly Random Rand = new();
+
+        private static ListNode CreateNode(ListNode prevNode, string data)
+        {
+            var node = new ListNode
+            {
+                Prev = prevNode,
+                Data = data
+            };
+            return node;
+        }
+
+        private static void AppendNode(ListRand list, ListNode newNode)
+        {
+            list.Tail.Next = newNode;
+            newNode.Prev = list.Tail;
+            newNode.Next = null;
+            list.Tail = newNode;
+            list.Count++;
+        }
+
+        private static void AssignRandomNodeFromList(ListRand list, ListNode node)
+        {
+            if (list.Count < 2)
+                return;
+
+            if (Rand.Next(0, 2) == 0)
+                return;
+
+            var rand = node;
+            while (rand == node)
+            {
+                rand = GetRandomNodeFromList(list);
+            }
+  
+            node.Rand = rand;
+        }
+
+        private static ListNode GetRandomNodeFromList(ListRand list)
+        {
+            var random = Rand.Next(0, list.Count);
+            var currentNode = list.Head;
+            var step = 1;
+            while (step++ < random)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            return currentNode;
+        }
+
+        private static string CreateString(int length = 8)
+        {
+            var builder = new StringBuilder();
+
+            for (var i = 0; i < length; i++)
+            {
+                var flt = Rand.NextDouble();
+                var shift = Convert.ToInt32(Math.Floor(25 * flt));
+                var letter = Convert.ToChar(shift + 65);
+                builder.Append(letter);
+            }
+
+            return builder.ToString();
+        }
+
+        private static bool CompareLists(ListRand list1, ListRand list2)
+        {
+            if (list1.Count != list2.Count)
+                return false;
+
+            var node1 = list1.Head;
+            var node2 = list2.Head;
+
+            for (var i = 0; i < list1.Count; i++)
+            {
+                if (node1.Data != node2.Data || (node1.Rand != node2.Rand && node1.Rand.Data != node2.Rand.Data))
+                    return false;
+
+                node1 = node1.Next;
+                node2 = node2.Next;
+            }
+
+            return true;
+        }
+
+        public static void Main()
+        {
+            var originalList = new ListRand();
+            var head = CreateNode(null, CreateString());
+            originalList.Head = originalList.Tail = head;
+            originalList.Count = 1;
+
+            var random = Rand.Next(9, 99);
+            for (var i = 0; i < random; i++)
+            {
+                var node = CreateNode(originalList.Tail, CreateString());
+                AppendNode(originalList, node);
+            }
+
+            var current = originalList.Head;
+            while (current != null)
+            {
+                AssignRandomNodeFromList(originalList, current );
+                current = current.Next;
+            }
+
+            using (var fs = new FileStream("ListRand.srl", FileMode.Create))
+            {
+                originalList.Serialize(fs);
+            }
+
+            var serializedList = new ListRand();
+            using (var fs = new FileStream("ListRand.srl", FileMode.Open))
+            {
+                serializedList.Deserialize(fs);
+            }
+
+            Console.WriteLine(CompareLists(originalList, serializedList)
+                ? "The data match"
+                : "The data does not match");
+            Console.ReadKey();
+        }
+    }
+}
